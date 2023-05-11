ab79ca9bb6ace08e9e6de661e4e56df889ea4885
Valery Gantchev <v@lery.nl>
2023-05-11 17:43:38 +0200

    redis client

--- clients/redis.js
+++ clients/redis.js
@@ -1,7 +1,10 @@
 const { createClient } = require("redis");
 
 async function newRedisClient() {
-  const client = createClient('10.130.48.5', 6380);
+  const client = createClient({
+    // url: 'redis://127.0.0.1:6379'
+    url: 'redis://10.130.48.5:6379'
+  });
   client.on('error', err => console.log('Redis Client Error', err));
 
   await client.connect();
