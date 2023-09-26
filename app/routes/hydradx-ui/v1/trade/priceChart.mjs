d727eff6b69216d2adf0119bc371dc5c1c78a10c
Valery Gantchev <v@lery.nl>
2023-09-26 15:43:15 +0200

    Add hydradx-ui/v1/trade/price_chart

---
+++ app/routes/hydradx-ui/v1/trade/priceChart.mjs
@@ -0,0 +1,61 @@
+import yesql from "yesql";
+import path from "path";
+import dayjs from "dayjs";
+import { dirname } from "../../../../../variables.mjs";
+import { CACHE_SETTINGS } from "../../../../../variables.mjs";
+import { cachedFetch } from "../../../../../helpers/cache_helpers.mjs";
+
+const sqlQueries = yesql(path.join(dirname(), "queries/hydradx-ui/v1/trade"), {
+  type: "pg",
+});
+
+export const VALID_TIMEFRAMES = ["hourly", "daily"];
+
+export default async (fastify, opts) => {
+  fastify.route({
+    url: "/price_chart/:assetIn-:assetOut",
+    method: ["GET"],
+    schema: {
+      description: "Price chart (Omnipool) for the trade page.",
+      tags: ["hydradx-ui/v1"],
+      params: {
+        type: "object",
+        properties: {
+          assetIn: {
+            type: "string",
+            description: "Symbol for assetIn",
+          },
+          assetOut: {
+            type: "string",
+            description: "Symbol for assetOut",
+          },
+        },
+        required: ["assetIn", "assetOut"],
+      },
+    },
+    handler: async (request, reply) => {
+      const assetIn = request.params.assetIn;
+      const assetOut = request.params.assetOut;
+
+      const endOfDay = dayjs().endOf("day").format("YYYY-MM-DDTHH:mm:ss");
+
+      const sqlQuery = sqlQueries.priceChart({ assetIn, assetOut, endOfDay });
+
+      console.log("DGDGDGD");
+      console.log(endOfDay);
+      console.log(sqlQuery);
+
+      let cacheSetting = { ...CACHE_SETTINGS["hydradxUiV1TradePriceChart"] };
+      cacheSetting.key = cacheSetting.key + "_" + assetIn + "_" + assetOut;
+
+      const result = await cachedFetch(
+        fastify.pg,
+        fastify.redis,
+        cacheSetting,
+        sqlQuery
+      );
+
+      reply.send(JSON.parse(result));
+    },
+  });
+};
---
+++ queries/hydradx-ui/v1/trade/priceChart.sql
@@ -0,0 +1,35 @@
+-- priceChart
+
+WITH nor_trades AS (
+  SELECT 
+    timestamp,
+    block.height AS block,
+    args->>'who' AS who,
+    name AS operation,
+    token_metadata_in.symbol AS asset_in,
+    token_metadata_out.symbol AS asset_out,
+    (args->>'amountIn')::numeric / (10 ^ token_metadata_in.decimals) AS amount_in,
+    (args->>'amountOut')::numeric / (10 ^ token_metadata_out.decimals) AS amount_out
+  FROM event
+  INNER JOIN block ON block_id = block.id
+  INNER JOIN token_metadata AS token_metadata_in ON (args->>'assetIn')::integer = token_metadata_in.id
+  INNER JOIN token_metadata AS token_metadata_out ON (args->>'assetOut')::integer = token_metadata_out.id
+  WHERE name IN ('Omnipool.BuyExecuted', 'Omnipool.SellExecuted')
+  AND timestamp BETWEEN '2023-01-06T13:00:00.000Z' AND :endOfDay
+),
+pair_price AS (
+  SELECT 
+    timestamp,
+    CASE 
+      WHEN asset_in = :assetIn AND asset_out = :assetOut THEN amount_in / amount_out
+      WHEN asset_in = :assetOut AND asset_out = :assetIn THEN amount_out / amount_in
+    END AS price
+  FROM nor_trades
+)
+SELECT
+  floor(extract(epoch from "timestamp")/3600)*3600 AS "time",
+  max(price) AS "price"
+FROM pair_price
+WHERE price IS NOT NULL
+GROUP BY 1
+ORDER BY 1;
