cde605dfb2e031c9bf2f970cc83d6790fe439be3
green-jay <greenee@email.cz>
2023-10-09 13:19:44 +0200

    add current asset price endpoint

--- variables.mjs
+++ variables.mjs
@@ -66,6 +66,10 @@ export const CACHE_SETTINGS = {
     key: "hydradx-ui_v1_stats_current_volume",
     expire_after: 10 * 60,
   },
+  hydradxUiV1StatsCurrentPrice: {
+    key: "hydradx-ui_v1_stats_current_price",
+    expire_after: 10 * 60,
+  },
   defillamaV1Tvl: {
     key: "defillama_v1_tvl",
     expire_after: 10 * 60,
