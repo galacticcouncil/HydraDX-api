9006a365cac0263048d0915b658410ae99510b46
Valery Gantchev <v@lery.nl>
2024-05-29 15:43:57 +0200

    refactor sql and cache helpers

--- helpers/cache_helpers.mjs
+++ helpers/cache_helpers.mjs
@@ -1,5 +1,17 @@
-export async function cachedFetch(sqlClient, redisClient, cacheSetting, qry) {
+export async function fetchFromCache(redisClient, cacheSetting) {
   let cachedResult = await redisClient.get(cacheSetting.key);
+  return cachedResult;
+}
+
+export async function updateCache(redisClient, cacheSetting, json) {
+  await redisClient.set(cacheSetting.key, json);
+  await redisClient.expire(cacheSetting.key, cacheSetting.expire_after);
+
+  return json
+}
+
+export async function cachedFetch(sqlClient, redisClient, cacheSetting, qry) {
+  let cachedResult = await fetchFromCache(redisClient, cacheSetting)
 
   if (cachedResult == null) {
     cachedResult = await updateCacheFromSql(sqlClient, redisClient, cacheSetting, qry);
@@ -10,10 +22,9 @@ export async function cachedFetch(sqlClient, redisClient, cacheSetting, qry) {
 
 export async function updateCacheFromSql(sqlClient, redisClient, cacheSetting, qry) {
   const { rows } = await sqlClient.query(qry);
-  const cachedResult = JSON.stringify(rows);
+  const result = JSON.stringify(rows);
 
-  await redisClient.set(cacheSetting.key, cachedResult);
-  await redisClient.expire(cacheSetting.key, cacheSetting.expire_after);
+  await updateCache(redisClient, cacheSetting, result)
 
-  return cachedResult;
+  return result;
 }
