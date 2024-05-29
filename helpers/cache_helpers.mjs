9f8dcaa8dcb6b33a69ac5cfb28bcdfe570853029
Valery Gantchev <v@lery.nl>
2024-05-29 15:47:01 +0200

    prettify

--- helpers/cache_helpers.mjs
+++ helpers/cache_helpers.mjs
@@ -7,24 +7,34 @@ export async function updateCache(redisClient, cacheSetting, json) {
   await redisClient.set(cacheSetting.key, json);
   await redisClient.expire(cacheSetting.key, cacheSetting.expire_after);
 
-  return json
+  return json;
 }
 
 export async function cachedFetch(sqlClient, redisClient, cacheSetting, qry) {
-  let cachedResult = await fetchFromCache(redisClient, cacheSetting)
+  let cachedResult = await fetchFromCache(redisClient, cacheSetting);
 
   if (cachedResult == null) {
-    cachedResult = await updateCacheFromSql(sqlClient, redisClient, cacheSetting, qry);
+    cachedResult = await updateCacheFromSql(
+      sqlClient,
+      redisClient,
+      cacheSetting,
+      qry
+    );
   }
 
   return cachedResult;
 }
 
-export async function updateCacheFromSql(sqlClient, redisClient, cacheSetting, qry) {
+export async function updateCacheFromSql(
+  sqlClient,
+  redisClient,
+  cacheSetting,
+  qry
+) {
   const { rows } = await sqlClient.query(qry);
   const result = JSON.stringify(rows);
 
-  await updateCache(redisClient, cacheSetting, result)
+  await updateCache(redisClient, cacheSetting, result);
 
   return result;
 }
--- jobs/cache_hydration-web_stats_job.mjs
+++ jobs/cache_hydration-web_stats_job.mjs
@@ -4,39 +4,48 @@ import { dirname, CACHE_SETTINGS } from "../variables.mjs";
 import { updateCache } from "../helpers/cache_helpers.mjs";
 import { fetchFromSql } from "../helpers/sql_helpers.mjs";
 
-const sqlQueries = yesql(path.join(dirname(), "queries/hydration-web/v1/stats"), {
-  type: "pg",
-});
+const sqlQueries = yesql(
+  path.join(dirname(), "queries/hydration-web/v1/stats"),
+  {
+    type: "pg",
+  }
+);
 
-export async function cacheHydrationWebStatsJob(
-  sqlClient,
-  redisClient
-) {
+export async function cacheHydrationWebStatsJob(sqlClient, redisClient) {
   let cacheSetting = { ...CACHE_SETTINGS["hydrationWebV1Stats"] };
 
   let result = {};
 
   // tvl
-  const tvlQuery = await fetchFromSql(sqlClient, sqlQueries.statsTvl({ asset: null }));
-  const tvl = tvlQuery.map((x) => x['tvl_usd']).reduce((sum, x) => sum + x);
-  result['tvl'] = tvl;
+  const tvlQuery = await fetchFromSql(
+    sqlClient,
+    sqlQueries.statsTvl({ asset: null })
+  );
+  const tvl = tvlQuery.map((x) => x["tvl_usd"]).reduce((sum, x) => sum + x);
+  result["tvl"] = tvl;
 
   // vol_30d
   const vol_30dQuery = await fetchFromSql(sqlClient, sqlQueries.statsVol30d());
-  const vol_30d = vol_30dQuery[0]['volume_usd'];
-  result['vol_30d'] = vol_30d;
+  const vol_30d = vol_30dQuery[0]["volume_usd"];
+  result["vol_30d"] = vol_30d;
 
   // xcm_vol_30d
   const xcm_vol_30d = JSON.parse(vol_30d) / 2;
-  result['xcm_vol_30d'] = xcm_vol_30d;
+  result["xcm_vol_30d"] = xcm_vol_30d;
 
   // assets_count
-  const assetsCount = await fetchFromSql(sqlClient, sqlQueries.statsAssetsCount());
-  result['assets_count'] = assetsCount[0]['count'];
+  const assetsCount = await fetchFromSql(
+    sqlClient,
+    sqlQueries.statsAssetsCount()
+  );
+  result["assets_count"] = assetsCount[0]["count"];
 
   // accounts_count
-  const accountsCount = await fetchFromSql(sqlClient, sqlQueries.statsAccountsCount());
-  result['accounts_count'] = accountsCount[0]['count'];
+  const accountsCount = await fetchFromSql(
+    sqlClient,
+    sqlQueries.statsAccountsCount()
+  );
+  result["accounts_count"] = accountsCount[0]["count"];
 
   await updateCache(redisClient, cacheSetting, JSON.stringify(result));
 
